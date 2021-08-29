290874a43e598e81ce070ac3b62bd5e85fce5e22
fselvino <fernandoselvino@gmail.com>
2021-08-29 10:59:48 -0300

    Dir e Help

---
+++ dir_e_help.py
@@ -0,0 +1,11 @@
+"""
+Utilitarios Python para te auxiliar na programão.
+
+dir -> Apresenta todos os atributos/propriedades e funções/metodos disponíveis para determinado tipo de dado ou variável.
+dir(tipo de dado/variavel)
+
+help -> Apresenta a documentoação/Como utilizar os atributos/propriedades e funções/metodos disponiveis
+para determinado tipo de dado ou variavel
+
+help(tipo de dado/variavel.propriedade)
+"""
